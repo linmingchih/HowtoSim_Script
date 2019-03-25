491737eb5c81d1535e852a90472a842b26e82da2
Lin, Ming Chih <mingchih.lin8@gmail.com>
2019-03-25 16:33:35 +0800

    Add files via upload

--- CatchView.py
+++ CatchView.py
@@ -2,29 +2,38 @@ oProject = oDesktop.GetActiveProject()
 oDesign = oProject.GetActiveDesign()  
 oEditor = oDesign.SetActiveEditor("3D Modeler")
 
-import collections
+import collections, os
 class CatchView():
-    def __init__(self, report_name, path):
-        self.image_list=[]
-        
-        for v in self.initViews():
+    def __init__(self, path, report_name):
+        self.path=path
+        self.pages=collections.OrderedDict()        
+        self.v=self.initViews()
+        self.dir='{}/{}'.format(path,report_name)
+        try:
+            os.mkdir(self.dir)
+        except:
+            pass
+        self.html='{}/{}.html'.format(path, report_name)
+    
+    def savePage(self, title):
+        self.pages[title]=[]
+        for v in self.v:
             oEditor.SetTopDownViewDirectionForActiveView(v)
-            imahge_path='{}{}_{}.png'.format(path,report_name,v)
-            self.saveImage(imahge_path)
-            self.image_list.append((v,imahge_path))
-        
-        self.genhtml('{}{}.html'.format(path,report_name))
-        
+            image_path='{}/{}_{}.png'.format(self.dir, title, v)
+            self.saveImage(image_path)
+            self.pages[title].append((v,image_path))
+                
     def initViews(self):
         tostr=lambda *args: tuple(str(i) for i in args)
         
         views=collections.OrderedDict()
-        views['top']=tostr(0,0,-1,-1,0,0)
-        views['bottom']=tostr(0,0,-1,1,0,0)
-        views['front']=tostr(1,0,0,0,1,0)
-        views['back']=tostr(-1,0,0,0,1,0)
-        views['left']=tostr(0,0,-1,0,1,0)
-        views['right']=tostr(0,0,1,0,1,0)
+
+        views['front']=tostr(0,0,-1,0,1,0)
+        views['back']=tostr(0,0,-1,0,-1,0)
+        views['top']=tostr(1,0,0,0,1,0)
+        views['bottom']=tostr(-1,0,0,0,1,0)
+        views['left']=tostr(0,0,-1,-1,0,0)
+        views['right']=tostr(0,0,-1,1,0,0)      
         
         exsistingCS=oEditor.GetRelativeCoordinateSystems()
         
@@ -44,16 +53,20 @@ class CatchView():
         oEditor.ExportModelImageToFile(path, 600, 400, options)
                
     
-    def genhtml(self, html_file):
-        with open(html_file, 'w') as f:
-            for i,j in self.image_list:
-                f.writelines('<figcaption>view:{}</figcaption><img src="{}">'.format(i,j))
+    def genhtml(self):
+        with open(self.html, 'w') as f:
+            for k in self.pages:
+                f.writelines('<H2>{}</H2>'.format(k))
+                for i, j in self.pages[k]:
+                    f.writelines('<img src="{}" bgcolor="#E6E6FA">'.format(j))
                 
         import webbrowser, os
-        webbrowser.open('file://' + os.path.realpath(html_file))  
+        webbrowser.open('file://' + os.path.realpath(self.html))  
 
-CatchView('H_Field','d:/demo2/')    
-     
+cv=CatchView('d:/demo2', 'Testing2')
+cv.savePage('E_Field')
+#cv.savePage('H_Field')   
+cv.genhtml()
 
         
       
