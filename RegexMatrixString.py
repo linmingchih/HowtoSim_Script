59111985bc64940b7b4cd856f24283400db288b1
Lin, Ming Chih <mingchih.lin8@gmail.com>
2019-09-07 19:59:11 +0800

    Add files via upload

--- RegexMatrixString.py
+++ RegexMatrixString.py
@@ -13,26 +13,79 @@ ports=oModule.GetExcitations()[::2]
 
 x=[(i,j) for i in ports for j in ports]
 oDesktop.ClearMessages("", "", 2)
-AddWarningMessage(str(x))
+#AddWarningMessage(str(x))
 
 def TypeA():
-    '''return dB(S(pad1,pad1));dB(S(pad2,pad2)),...'''
     match=[]
     for i, j in x:
         try:
-            Ni=int(re.search('pad(\d+)', i).group(1))
-            Nj=int(re.search('pad(\d+)', j).group(1))
-            if Ni==Nj:
+            if i==j:
                 match.append((i,j))
         except:
             pass
-            
-    result=';'.join(['dB(S({},{}))'.format(i,j) for i,j in match])
-    
-    try:
-        AddWarningMessage(result)
+    if not match:
+        AddWarningMessage('No match, Please check condition!')
+        return None
+    else:
+        AddWarningMessage('{} matches!'.format(len(match)))
+        [AddWarningMessage(str(i)) for i in match]
+        result=';'.join(['dB(S({},{}))'.format(i,j) for i,j in match])
         Clipboard.SetText(result)
-    except:
-        AddWarningMessage('Failed!')
 
-TypeA()
+def TypeB():
+    match=[]
+    for i, j in x:
+        try:
+            mi=re.search('_(\d+)_.*S(\d+)', i)
+            mj=re.search('_(\d+)_.*S(\d+)', j)
+            if mi.group(1)!=mj.group(1) and mi.group(2)==mj.group(2):
+                match.append((i,j))
+        except:
+            pass
+    if not match:
+        AddWarningMessage('No match, Please check condition!')
+        return None
+    else:
+        AddWarningMessage('{} matches!'.format(len(match)))
+        [AddWarningMessage(str(i)) for i in match]
+        result=';'.join(['dB(S({},{}))'.format(i,j) for i,j in match])
+        Clipboard.SetText(result)
+        
+def TypeC():
+    match=[]
+    for i, j in x:
+        try:
+            mi=re.search('_(\d+)_.*S(\d+)', i)
+            mj=re.search('_(\d+)_.*S(\d+)', j)
+            if mi.group(1)==mj.group(1) and (mi.group(2),mj.group(2)) in [('3','4'),('7','8'),('9','10'),('14','15')]:
+                match.append((i,j))
+        except:
+            pass
+    if not match:
+        AddWarningMessage('No match, Please check condition!')
+        return None
+    else:
+        AddWarningMessage('{} matches!'.format(len(match)))
+        [AddWarningMessage(str(i)) for i in match]
+        result=';'.join(['dB(S({},{}))'.format(i,j) for i,j in match])
+        Clipboard.SetText(result)
+
+def TypeD():
+    match=[]
+    for i, j in x:
+        try:
+            mi=re.search('_(\d+)_.*S(\d+)', i)
+            mj=re.search('_(\d+)_.*S(\d+)', j)
+            if mi.group(1)==mj.group(1) and (mi.group(2),mj.group(2)) in [('3','4'),('7','8'),('9','10'),('14','15')]:
+                match.append((i,j))
+        except:
+            pass
+    if not match:
+        AddWarningMessage('No match, Please check condition!')
+        return None
+    else:
+        AddWarningMessage('{} matches!'.format(len(match)))
+        [AddWarningMessage(str(i)) for i in match]
+        result=';'.join(['mag(Z({},{}))'.format(i,j) for i,j in match])
+        Clipboard.SetText(result)
+TypeD()
