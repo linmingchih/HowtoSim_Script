7bd2fcacb7871614df1d0be0f743a9336131b3d3
Lin, Ming Chih <mingchih.lin8@gmail.com>
2019-09-08 12:37:49 +0800

    Add files via upload

--- RegexMatrixString.py
+++ RegexMatrixString.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 10:43:23 2019
+
+@author: mlin
+For HFSS and HFSS 3D Layout
+"""
+
 import re, clr
 clr.AddReference('System.Windows.Forms')
 from System.Windows.Forms import Clipboard
@@ -7,13 +15,18 @@ ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
 oDesktop.RestoreWindow()
 oProject = oDesktop.GetActiveProject()
 oDesign = oProject.GetActiveDesign()
+oDesktop.ClearMessages("", "", 2) 
 
-oModule = oDesign.GetModule("BoundarySetup")
-ports=oModule.GetExcitations()[::2]
+try:
+    oModule = oDesign.GetModule("BoundarySetup")
+    ports=oModule.GetExcitations()[::2]
+except:
+    oModule = oDesign.GetModule("Excitations")
+    ports=oModule.GetAllPortsList()
 
+AddWarningMessage(str(ports))    
 x=[(i,j) for i in ports for j in ports]
-oDesktop.ClearMessages("", "", 2)
-#AddWarningMessage(str(x))
+
 
 def TypeA():
     match=[]
@@ -31,6 +44,7 @@ def TypeA():
         [AddWarningMessage(str(i)) for i in match]
         result=';'.join(['dB(S({},{}))'.format(i,j) for i,j in match])
         Clipboard.SetText(result)
+
 
 def TypeB():
     match=[]
@@ -88,4 +102,21 @@ def TypeD():
         [AddWarningMessage(str(i)) for i in match]
         result=';'.join(['mag(Z({},{}))'.format(i,j) for i,j in match])
         Clipboard.SetText(result)
-TypeD()
+
+def TypeE():
+    match=[]
+    for i, j in x:
+        try:
+            if i[-8:]==j[-8:] and i.startswith('BGA') and j.startswith('FCHIP'):
+                match.append((i,j))
+        except:
+            pass
+    if not match:
+        AddWarningMessage('No match, Please check condition!')
+        return None
+    else:
+        AddWarningMessage('{} matches!'.format(len(match)))
+        [AddWarningMessage(str(i)) for i in match]
+        result=';'.join(['dB(S({},{}))'.format(i,j) for i,j in match])
+        Clipboard.SetText(result)
+TypeE()
