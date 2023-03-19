8f6abbcd9c2ad8c76e97357cd622f9f2ca87da7c
Lin, Ming Chih <mingchih.lin8@gmail.com>
2023-03-19 17:35:53 +0800

    Add files via upload

--- 2022/Rename_Net_2.py
+++ 2022/Rename_Net_2.py
@@ -1,7 +1,8 @@
 import json, os
+from collections import defaultdict
 
 # from win32com import client
-# oApp = client.Dispatch("Ansoft.ElectronicsDesktop.2022.1")
+# oApp = client.Dispatch("Ansoft.ElectronicsDesktop.2022.2")
 # oDesktop = oApp.GetAppDesktop()
 # oDesktop.RestoreWindow()
 
@@ -10,35 +11,48 @@ oProject = oDesktop.GetActiveProject()
 oDesign = oProject.GetActiveDesign()
 oEditor = oDesign.SetActiveEditor("3D Modeler")
 os.chdir(os.path.dirname(__file__))
+
 unit = oEditor.GetModelUnits()
+unit_map = {'nm':1e-9, 'um':1e-6, 'mm':1e-3, 'mil':2.54e-5}
+scale = unit_map[unit]
+
+
 with open('rename.json') as f:
     layer_info, db = json.load(f)
 
-result = {}
+result = defaultdict(list)
+obj_net = defaultdict(set)
+
 for layer in db:
     z = layer_info[layer]
     for obj in db[layer]:
         x, y, net, otype = obj
-        if net not in result:
-            result[net] = []
         names = oEditor.GetBodyNamesByPosition(["NAME:Parameters", 
-                                                "XPosition:=","{:.6f}{}".format(x, unit), 
-                                                "YPosition:=","{:.6f}{}".format(y, unit), 
-                                                "ZPosition:=","{:.6f}{}".format(z, unit)
+                                                "XPosition:=","{:.6f}{}".format(x/scale, unit), 
+                                                "YPosition:=","{:.6f}{}".format(y/scale, unit), 
+                                                "ZPosition:=","{:.6f}{}".format(z/scale, unit)
                                                 ])
+
         for name in names:
-            if 'fill' in name or 'airbox' in name or 'UNNAMED' in name:
-                continue
-            if name not in result[net]:
-                result[net].append(name)
+            result[net].append(name)
+            obj_net[name].add(net)
+
+new_result = defaultdict(list)
+for net, names in result.items():
+    for name in names:
+        if len(obj_net[name]) == 1:
+            new_result[net].append(name)
 
 
-total = len(result)
+total = len(new_result)
+#%%
+
 n = 0
-for net in result:
+for net in new_result:
     oDesktop.ClearMessages("","",2)
     n+=1
     oDesktop.AddMessage(oProject.GetName(), oDesign.GetName(),0,'{}/{}'.format(n,total))
+    
     try:
         oEditor.ChangeProperty(
             [
@@ -47,7 +61,7 @@ for net in result:
                     "NAME:Geometry3DAttributeTab",
                     [
                         "NAME:PropServers"                    
-                    ] + result[net],
+                    ] + new_result[net],
                     [
                         "NAME:ChangedProps",
                         [
@@ -60,5 +74,5 @@ for net in result:
     except:
         pass
 
-AddWarningMessage(str(result['GND']))
+
 oDesktop.AddMessage(oProject.GetName(), oDesign.GetName(),0,'Completed!')
