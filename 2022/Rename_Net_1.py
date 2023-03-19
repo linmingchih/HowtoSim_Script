3db85f7b4467a6dd2ff884d203a32f09591d7a97
Lin, Ming Chih <mingchih.lin8@gmail.com>
2023-03-19 17:30:03 +0800

    Add files via upload

--- 2022/Rename_Net_1.py
+++ 2022/Rename_Net_1.py
@@ -1,16 +1,22 @@
-# ----------------------------------------------
-# Script Recorded by ANSYS Electronics Desktop Version 2021.1.0
-# 13:27:00  Apr 16, 2021
-# ----------------------------------------------
-import ScriptEnv
+# from win32com import client
+# oApp = client.Dispatch("Ansoft.ElectronicsDesktop.2022.2")
+# oDesktop = oApp.GetAppDesktop()
+# oDesktop.RestoreWindow()
+# AddWarningMessage = print
+
+
 import json, os
-ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
-oDesktop.RestoreWindow()
+
 oDesktop.ClearMessages("","",2)
 oProject = oDesktop.GetActiveProject()
 oDesign = oProject.GetActiveDesign()
 oEditor = oDesign.SetActiveEditor("Layout")
-os.chdir(os.path.dirname(__file__))
+folder = os.chdir(os.path.dirname(__file__))
+folder = r'D:\OneDrive - ANSYS, Inc\Customer2023\2023_03_19_vianexech_renaming'
+
+unit_map = {'nm':1e-9, 'um':1e-6, 'mm':1e-3, 'mil':2.54e-5}
+scale = unit_map[oEditor.GetActiveUnits()]
+
 db = {}
 layerinfo = {}
 for layer in oEditor.GetStackupLayerNames():
@@ -18,9 +24,14 @@ for layer in oEditor.GetStackupLayerNames():
     if 'Type: signal' in info:
         lower_elevation = info[-1].split(':')[-1].strip()
         try:
-            x = float(lower_elevation[:-2])
+            value = lower_elevation[:-2]
+            unit = lower_elevation[-2:]
+            x = float(value)*unit_map[unit]
         except:
-            x = float(lower_elevation[:-3])
+            value = lower_elevation[:-3]
+            unit = lower_elevation[-3:]
+            x = float(value)*unit_map[unit]
+        
         layerinfo[layer] = x
         db[layer] = []
     else:
@@ -35,10 +46,10 @@ for layer in oEditor.GetStackupLayerNames():
         else:
             continue
         
-        x, y = [float(j) for j in location.split(',')]
+        x, y = [float(j)*scale for j in location.split(',')]
         net = oEditor.GetPropertyValue('BaseElementTab', i, 'Net')
         db[layer].append((x, y, net, otype))
 
-with open('rename.json', 'w') as f:
+with open(os.path.join(folder, 'rename.json'), 'w') as f:
     json.dump((layerinfo, db), f, indent=4)
     AddWarningMessage('Completed!')
